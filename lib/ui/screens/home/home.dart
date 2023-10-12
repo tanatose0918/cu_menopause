d996b0657b6e68f9239b90ef920b49151ddf0a72
tanatose0918 <46656074+tanatose0918@users.noreply.github.com>
2023-10-12 16:06:29 +0700

    change stateless to stateful

--- lib/ui/screens/home/home.dart
+++ lib/ui/screens/home/home.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Center(
